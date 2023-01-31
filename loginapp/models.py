a0949ce6ad31b754f3d1a98fc7eaafc24841bbbe
Keniel380 <107254722+Keniel380@users.noreply.github.com>
2023-01-31 16:08:55 +0300

    Update models.py

--- loginapp/models.py
+++ loginapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class loginapp(models.Model):
    First_name = models.fields.CharField(max_length=50)
    Last_name = models.fields.CharField(max_length=50)
-   Gender = models.fields.CharField(max_length=4)
+   Gender = models.fields.CharField(max_length=5, blank= True, null=True)
    Email = models.fields.CharField(max_length=50)
    Password = models.fields.CharField(max_length=50)
    Re_password = models.fields.CharField(max_length=50)
